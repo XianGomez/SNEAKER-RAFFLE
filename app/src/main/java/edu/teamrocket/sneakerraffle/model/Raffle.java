371654f9a56cf5b4d2785ac0819c02005b9c2492
XianGomez <xiiangomezz@gmail.com>
2026-06-15 14:29:12 +0200

    feat(Raffle): Añadir metodo register, totalEntries y listEntries

--- app/src/main/java/edu/teamrocket/sneakerraffle/model/Raffle.java
+++ app/src/main/java/edu/teamrocket/sneakerraffle/model/Raffle.java
@@ -1,9 +1,14 @@
 package edu.teamrocket.sneakerraffle.model;
 
+import java.util.List;
 
 public interface Raffle {
 
     void sizesRun(Sizes minSize, Sizes maxSize);
     double price();
+    void register(Entry entry);
+    void register(Entry entry1, Entry entry2, Entry entry3);
+    int totalEntries();
+    List<String> listEntries();
 
 }
