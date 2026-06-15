78ad038425e0e8aeb1dc379b788c17043183c09f
XianGomez <xiiangomezz@gmail.com>
2026-06-15 15:31:33 +0200

    fix(App): Agregar importaciones faltantes

--- app/src/main/java/edu/teamrocket/sneakerraffle/App.java
+++ app/src/main/java/edu/teamrocket/sneakerraffle/App.java
@@ -1,6 +1,8 @@
 package edu.teamrocket.sneakerraffle;
 
 import edu.teamrocket.sneakerraffle.model.Entry;
+import edu.teamrocket.sneakerraffle.model.Payment;
+import edu.teamrocket.sneakerraffle.model.Paypal;
 import edu.teamrocket.sneakerraffle.model.Raffle;
 import edu.teamrocket.sneakerraffle.model.Sizes;
 import edu.teamrocket.sneakerraffle.model.Sneaker;
