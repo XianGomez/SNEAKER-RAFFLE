3d72894601340b64b4e064968231e8fe25db168d
XianGomez <xiiangomezz@gmail.com>
2026-06-15 13:36:29 +0200

    feat(Sneaker): Añadir clase Sneaker con constructor y getters setter y metodo sizeRun

---
+++ app/src/main/java/edu/teamrocket/sneakerraffle/model/Sneaker.java
@@ -0,0 +1,59 @@
+package edu.teamrocket.sneakerraffle.model;
+
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+
+public class Sneaker implements Raffle {
+    private String nombre;
+    private String estilo;
+    private double precio;
+    private EnumSet<Sizes> listaSizes;
+
+
+    public Sneaker(String nombre, String estilo, double precio) {
+        this.nombre = nombre;
+        this.estilo = estilo;
+        this.precio = precio;
+    }
+
+    public String getEstilo() {
+        return estilo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setEstilo(String estilo) {
+        this.estilo = estilo;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+
+    public void sizesRun(Sizes minSize, Sizes maxSize) {
+        EnumSet<Sizes> listaSizes = EnumSet.range(minSize, maxSize);
+        this.listaSizes = listaSizes;
+    }
+
+    public EnumSet<Sizes> getListaSizes() {
+        return listaSizes;
+    }
+
+    @Override
+    public String toString() {
+        return "\n\t\t"+ this.getNombre() + "\n\t\t" + this.getEstilo()
+               + "\n\t\t" + this.getPrecio()+"$\n\t\t" + this.getListaSizes();
+    }
+}
